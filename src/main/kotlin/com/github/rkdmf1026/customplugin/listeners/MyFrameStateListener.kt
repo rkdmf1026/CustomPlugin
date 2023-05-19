ddcd60a5d6f68b6357fbbf40f113bb228e58ce0b
GitHub Action <action@github.com>
2023-05-19 10:47:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/rkdmf1026/customplugin/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/com/github/rkdmf1026/customplugin/listeners/MyFrameStateListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.rkdmf1026.customplugin.listeners
 
 import com.intellij.ide.FrameStateListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/rkdmf1026/customplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/rkdmf1026/customplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.rkdmf1026.customplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.rkdmf1026.customplugin.MyBundle
+import com.github.rkdmf1026.customplugin.services.MyProjectService
 import javax.swing.JButton
 
 
