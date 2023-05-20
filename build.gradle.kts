2ee3924d336d0fd189e7bae2434ea17916a54fd6
Lee <rkdmf1026@naver.com>
2023-05-20 12:44:43 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,9 @@ repositories {
 // Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
 dependencies {
 //    implementation(libs.annotations)
+    implementation("com.intellij.openapi:openapi:2022.2")
+    implementation("com.intellij.openapi:extensions:2022.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
 }
 
 // Set the JVM language level used to build the project. Use Java 11 for 2020.3+, and Java 17 for 2022.2+.
